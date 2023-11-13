0a368ff4885875735c535f23074fe8e1f66429a7
matsuchiyo <macho.macho.dev@gmail.com>
2023-11-13 11:30:15 +0900

    text baseline 2

--- lib/text_baseline.dart
+++ lib/text_baseline.dart
@@ -33,28 +33,26 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(title: Text(widget.title)),
       body: SafeArea(
-        child: Center(
+        child: Container(
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.all(16),
           child: Container(
             padding: const EdgeInsets.symmetric(
-              vertical: 4,
-              // vertical: 3,
-              horizontal: 8,
+              vertical: 8,
+              horizontal: 16,
             ),
             decoration: const BoxDecoration(
               color: Color(0xFFD9D9D9),
-              borderRadius: BorderRadius.all(Radius.circular(12)),
+              borderRadius: BorderRadius.all(Radius.circular(32)),
             ),
             child: const Text(
-              // '1セット(¥2,000/1kg)を購入する ',
-              'ABCDEFGabcdefg',
+              'ABCDefgh',
               style: TextStyle(
                 fontFamily: 'NotoSans',
                 fontWeight: FontWeight.w400,
-                fontSize: 16,
-                height: 16.0 / 16.0,
-                // height: 18.0 / 16.0,
+                fontSize: 48,
+                height: 48.0 / 48.0,
                 leadingDistribution: TextLeadingDistribution.even,
               ),
             ),
